ad924600acdb30b45c769265ec8b395ca5dcd75d
Lyka-Martinez <martinezlykamae22@gmail.com>
2025-05-12 01:16:28 +0800

    fix: dynamically set base path for Vercel and GitHub Pages deployment - 01.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [
-        react(), 
-        tailwindcss()
-    ],
-    base: '/virtual-tour/'
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
+        plugins: [
+            react(),
+            tailwindcss(),
+        ],
+        define: {
+            __APP_ENV__: JSON.stringify(env.APP_ENV),
+        },
+        base: env.VERCEL === '1' ? '/' : '/virtual-tour/'   // Set base dynamically based on VERCEL env variable
+    }
 })
